cd83fe889e3eddcd269b08afecf23b1b330f8c78
davidfa71 <52572302+davidfa71@users.noreply.github.com>
2020-02-08 00:25:19 +0100

    Update runSPLOT.sh

--- runSPLOT.sh
+++ runSPLOT.sh
@@ -9,4 +9,4 @@ rm ${1%.xml}-XOR.log >/dev/null 2>&1
 mv ${1%.xml}.exp ${1%.xml}-XOR.exp
 ../code/bin/Logic2BDD -line-length 70 -min-nodes 100000 -constraint-reorder minspan -base ${1%.xml}-XOR  -cudd -no-static-comp ${1%.xml}.var ${1%.xml}-XOR.exp >>${1%.xml}-XOR.log 2>&1 
 wait
-rm *.data *.reorder *.dddmp >/dev/null 2>&1 
+rm *.data *.reorder >/dev/null 2>&1 
